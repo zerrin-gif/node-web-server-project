ff994d519ee43a37bca16c69eb7da8634d3a19b3
zerrin-gif <72238583+zerrin-gif@users.noreply.github.com>
2021-03-17 20:26:16 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -2,19 +2,58 @@ const http = require("http");
 
 const host = 'localhost'; 
 
-const port = 8000; 
+const port = 5000; 
 
 const requestListener = function (req, res) { 
 
-    // res.writeHead(200); 
-    res.setHeader("Content-Type", "text/html"); 
-    // res.end("My first server!"); 
-    res.end(`<html><body><h1>This is HTML</h1></body></html>`); 
-}; 
+    res.setHeader("Content-Type", "application/json"); 
+
+    switch (req.url) {
+        case "/": 
+
+            res.writeHead(200); 
+
+            res.write(`This is home Page`); 
+
+            break; 
+            case "/student": 
+
+            res.writeHead(200); 
+
+            res.write(`This is student Page`); 
+
+            break; 
+            case "/admin": 
+
+            res.writeHead(200); 
+
+            res.write(`This is admin Page`); 
+            
+            break; 
+            case "/data": 
+
+            res.writeHead(200,{'Content-Type': 'application/json'}); 
+
+            res.write (JSON.stringify({ message: "This is a message from web server!"})); 
+            
+            break; 
+            default: 
+
+            res.writeHead(404); 
+
+            res.end(JSON.stringify({error:"Resource not found"}));
+            
+  
+    } 
+    res.end(); 
+
+} 
 
 const server = http.createServer(requestListener); 
+
 server.listen(port, host, () => { 
 
     console.log(`Server is running on http://${host}:${port}`); 
 
 }); 
+
