44358ec3d28fb2f1471cf67a0a773f2b0d5a642a
zerrin-gif <72238583+zerrin-gif@users.noreply.github.com>
2021-03-18 21:57:09 -0500

    Add files via upload

---
+++ deleteRequest.js
@@ -0,0 +1,33 @@
+const https = require('https');
+
+const options = {
+  method: 'DELETE',
+  host: 'jsonplaceholder.typicode.com',
+  path: '/users/1',
+  headers: {
+    'Accept': 'application/json'
+  }
+};
+
+let request = https.request(options,(res) => {
+  if (res.statusCode !== 200) {
+    console.error(`Did not get an OK from the server. Code: ${res.statusCode}`);
+    res.resume();
+    return;
+  }
+  let data = '';
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('close', () => {
+    console.log('Retrieved all data');
+    console.log(JSON.parse(data));
+  });
+  
+ });
+ request.end()
+ request.on('error', (err) => {
+  console.error(`Encountered an error trying to make a request: ${err.message}`);
+});
